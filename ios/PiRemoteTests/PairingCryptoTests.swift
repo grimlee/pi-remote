bb8907a619fff7e6b0a2b537cb2d98506fe745e6
grimlee <62026457+grimlee@users.noreply.github.com>
2026-09-18 08:00:17 +0800

    test: add cross-language pairing crypto vector

---
+++ ios/PiRemoteTests/PairingCryptoTests.swift
@@ -0,0 +1,52 @@
+import CryptoKit
+import XCTest
+@testable import PiRemote
+
+final class PairingCryptoTests: XCTestCase {
+    func testPairingVectorMatchesHostImplementation() throws {
+        let device = DevicePublicIdentity(
+            id: "device_testvector",
+            name: "iPhone",
+            signingPublicKey: "yM6EbQV4R3kp5yy_dnkBqsme8jA9ypGFMbhwAvc6hZc",
+            keyAgreementPublicKey: "OHWQojocMsWFkAFfLZ0XtemKwhoYP_h0kv3KIN8FM34"
+        )
+
+        let message = PairingCrypto.requestMessage(
+            pairingId: "pair_testvector",
+            machineId: "machine_testvector",
+            device: device
+        )
+
+        XCTAssertEqual(
+            message.base64URLEncodedString(),
+            "cGlyZW1vdGUtcGFpci1yZXF1ZXN0LXYxAHBhaXJfdGVzdHZlY3RvcgBtYWNoaW5lX3Rlc3R2ZWN0b3IAZGV2aWNlX3Rlc3R2ZWN0b3IAaVBob25lAHlNNkViUVY0UjNrcDV5eV9kbmtCcXNtZThqQTl5cEdGTWJod0F2YzZoWmMAT0hXUW9qb2NNc1dGa0FGZkxaMFh0ZW1Ld2hvWVBfaDBrdjNLSU44Rk0zNA"
+        )
+
+        XCTAssertEqual(
+            try PairingCrypto.proof(
+                secretBase64URL: "ERERERERERERERERERERERERERERERERERERERERERE",
+                message: message
+            ),
+            "dNuxN6BRlMe0IZ_Mx0vQrUeI9yYI7sPZIZlXIjvKFHE"
+        )
+
+        let privateKeyRaw = try XCTUnwrap(
+            Data(base64URLEncoded: "ZROu6neDb7zKDoTNlziatRJ-rt9vudG1KuBsQ0Egizo")
+        )
+        let privateKey = try Curve25519.Signing.PrivateKey(
+            rawRepresentation: privateKeyRaw
+        )
+        XCTAssertEqual(
+            privateKey.publicKey.rawRepresentation.base64URLEncodedString(),
+            device.signingPublicKey
+        )
+
+        XCTAssertEqual(
+            try PairingCrypto.deviceSignature(
+                signingPrivateKeyRaw: privateKeyRaw,
+                message: message
+            ),
+            "PzsKk2gTlKX9WgUEv-fpIL169aPcnSHXo9AG9gJOas-6aURwpX9Gj3_h4W4Lr9FuRTZ6wrFWCmhK-hLhDpNTAw"
+        )
+    }
+}
